19f85f46fafb6b0db45141e30ad3954f74d1fc81
Степан <@LAPTOP-B0PF4KGO>
2023-07-04 14:04:35 +0500

    Refactor

--- BlazorApp1/Shared/Models/UserModel.cs
+++ BlazorApp1/Shared/Models/UserModel.cs
@@ -4,6 +4,4 @@ public record UserModel
 {
     public int Id { get; set; }
     public required string Name { get; set; }
-    public MessageModel[] Messages { get; set; } = Array.Empty<MessageModel>();
-    public ChatroomModel[] Chatrooms { get; set; } = Array.Empty<ChatroomModel>();
 }
